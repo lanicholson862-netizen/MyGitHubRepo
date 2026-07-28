865041bd6f1d9b0277b0704e1b2506e6521c1d19
lanicholson862-netizen <lanicholson862@gmail.com>
2026-07-28 08:23:33 +0800

    Created Orders Table

--- project.py
+++ project.py
@@ -5,3 +5,10 @@ DB_Name = "project.db"
 conn = sqlite3.connect(DB_Name)
 cursor = conn.cursor()
 
+conn.execute("""
+    CREATE TABLE IF NOT EXISTS Orders(
+        orderID INTEGER PRIMARY KEY,
+        dishID INTEGER NOT NULL,
+        FOREIGN KEY(dishID) REFERENCES Dish(dishID)
+    )
+""")
